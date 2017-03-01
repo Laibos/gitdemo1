44aff3addd2f591511b8708d92da9793f6a57be8
laijy <858039238@qq.com>
2017-03-01 15:53:04 +0800

    demo1 for branch

---
+++ src/main/java/Demo2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/3/1.
+ */
+public class Demo2 {
+    public static void main(String[] args){
+        System.out.print("It a demo for brance test");
+        System.out.print("It's not success at the first time.");
+    }
+}

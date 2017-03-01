6b7d5b178be9ac6cba5ef1ea98a26239b643bbc1
laijy <858039238@qq.com>
2017-03-01 15:29:18 +0800

    upload success

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -5,5 +5,6 @@ public class Demo1 {
     public static void main(String[] args){
         System.out.print("Demo1");
         System.out.print("What the matter?");
+        System.out.print("It sames that it's has got success!");
     }
 }
